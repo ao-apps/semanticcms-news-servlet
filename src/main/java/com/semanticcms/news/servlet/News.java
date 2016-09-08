8751d5a4262019a6f5663f716af70df6d07b137c
AO Industries, Inc. <support@aoindustries.com>
2016-09-08 11:42:32 -0500

    Adding newsfeeds

---
+++ src/main/java/com/semanticcms/news/servlet/News.java
@@ -0,0 +1,175 @@
+/*
+ * semanticcms-news-servlet - SemanticCMS newsfeeds in a Servlet environment.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-news-servlet.
+ *
+ * semanticcms-news-servlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-news-servlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-news-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.news.servlet;
+
+import com.aoindustries.io.TempFileList;
+import com.aoindustries.io.buffer.AutoTempFileWriter;
+import com.aoindustries.io.buffer.BufferResult;
+import com.aoindustries.io.buffer.BufferWriter;
+import com.aoindustries.io.buffer.SegmentedWriter;
+import com.aoindustries.servlet.filter.TempFileContext;
+import com.semanticcms.core.model.ElementContext;
+import com.semanticcms.core.servlet.CaptureLevel;
+import com.semanticcms.core.servlet.Element;
+import com.semanticcms.core.servlet.PageContext;
+import com.semanticcms.core.servlet.SemanticCMS;
+import com.semanticcms.news.servlet.impl.NewsImpl;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.SkipPageException;
+import org.joda.time.DateTime;
+import org.joda.time.ReadableDateTime;
+
+public class News extends Element<com.semanticcms.news.model.News> {
+
+	public News(
+		ServletContext servletContext,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		ReadableDateTime pubDate
+	) {
+		super(
+			servletContext,
+			request,
+			response,
+			new com.semanticcms.news.model.News()
+		);
+		element.setView(SemanticCMS.DEFAULT_VIEW_NAME);
+		element.setPubDate(pubDate);
+	}
+
+	public News(
+		ServletContext servletContext,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		DateTime pubDate,
+		String description
+	) {
+		this(servletContext, request, response, pubDate);
+		element.setDescription(description);
+	}
+
+	/**
+	 * Creates news in the current page context.
+	 *
+	 * @see  PageContext
+	 */
+	public News(DateTime pubDate) {
+		this(
+			PageContext.getServletContext(),
+			PageContext.getRequest(),
+			PageContext.getResponse(),
+			pubDate
+		);
+	}
+
+	/**
+	 * Creates news in the current page context.
+	 *
+	 * @see  PageContext
+	 */
+	public News(DateTime pubDate, String description) {
+		this(pubDate);
+		element.setDescription(description);
+	}
+
+	@Override
+	public News id(String id) {
+		super.id(id);
+		return this;
+	}
+
+	public News book(String book) {
+		element.setBook(book);
+		return this;
+	}
+
+	public News page(String page) {
+		element.setTargetPage(page);
+		return this;
+	}
+
+	public News element(String element) {
+		this.element.setElement(element);
+		return this;
+	}
+
+	public News view(String view) {
+		element.setView(view);
+		return this;
+	}
+
+	public News title(String title) {
+		element.setTitle(title);
+		return this;
+	}
+
+	public News description(String description) {
+		element.setDescription(description);
+		return this;
+	}
+
+	private BufferResult writeMe;
+	@Override
+	protected void doBody(CaptureLevel captureLevel, Body<? super com.semanticcms.news.model.News> body) throws ServletException, IOException, SkipPageException {
+		super.doBody(captureLevel, body);
+		BufferWriter capturedOut;
+		if(captureLevel == CaptureLevel.BODY) {
+			// Enable temp files if temp file context active
+			capturedOut = TempFileContext.wrapTempFileList(
+				new SegmentedWriter(),
+				request,
+				// Java 1.8: AutoTempFileWriter::new
+				new TempFileContext.Wrapper<BufferWriter>() {
+					@Override
+					public BufferWriter call(BufferWriter original, TempFileList tempFileList) {
+						return new AutoTempFileWriter(original, tempFileList);
+					}
+				}
+			);
+		} else {
+			capturedOut = null;
+		}
+		try {
+			NewsImpl.writeNewsImpl(
+				servletContext,
+				request,
+				response,
+				capturedOut,
+				element
+			);
+		} finally {
+			if(capturedOut != null) capturedOut.close();
+		}
+		writeMe = capturedOut==null ? null : capturedOut.getResult();
+	}
+
+	@Override
+	public void writeTo(Writer out, ElementContext context) throws IOException {
+		if(writeMe != null) writeMe.writeTo(out);
+	}
+}
---
+++ src/main/java/com/semanticcms/news/servlet/impl/NewsImpl.java
@@ -0,0 +1,174 @@
+/*
+ * semanticcms-news-servlet - SemanticCMS newsfeeds in a Servlet environment.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-news-servlet.
+ *
+ * semanticcms-news-servlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-news-servlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-news-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.news.servlet.impl;
+
+import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.encodeTextInXhtmlAttribute;
+import com.aoindustries.lang.NotImplementedException;
+import com.semanticcms.core.model.Element;
+import com.semanticcms.core.model.Node;
+import com.semanticcms.core.model.Page;
+import com.semanticcms.core.model.PageRef;
+import com.semanticcms.core.servlet.CaptureLevel;
+import com.semanticcms.core.servlet.CapturePage;
+import com.semanticcms.core.servlet.CurrentNode;
+import com.semanticcms.core.servlet.CurrentPage;
+import com.semanticcms.core.servlet.PageIndex;
+import com.semanticcms.core.servlet.PageRefResolver;
+import com.semanticcms.core.servlet.impl.LinkImpl;
+import com.semanticcms.news.model.News;
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+final public class NewsImpl {
+
+	public static void writeNewsImpl(
+		ServletContext servletContext,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		Appendable out,
+		News news
+	) throws ServletException, IOException {
+		// Get the current capture state
+		final CaptureLevel captureLevel = CaptureLevel.getCaptureLevel(request);
+		if(captureLevel.compareTo(CaptureLevel.META) >= 0) {
+			final Node currentNode = CurrentNode.getCurrentNode(request);
+			final Page currentPage = CurrentPage.getCurrentPage(request);
+			if(currentPage == null) throw new ServletException("news must be nested within a page");
+
+			final String element = news.getElement();
+
+			// Find the target page
+			PageRef currentPageRef = currentPage.getPageRef();
+			PageRef targetPageRef;
+			if(news.getBook() == null) {
+				if(news.getTargetPage() == null) {
+					targetPageRef = currentPageRef;
+				} else {
+					targetPageRef = PageRefResolver.getPageRef(
+						servletContext,
+						request,
+						null,
+						news.getTargetPage()
+					);
+				}
+			} else {
+				if(news.getTargetPage() == null) throw new ServletException("page required when book provided.");
+				targetPageRef = PageRefResolver.getPageRef(
+					servletContext,
+					request,
+					news.getBook(),
+					news.getTargetPage()
+				);
+			}
+			// Add page links
+			if(currentNode != null) currentNode.addPageLink(targetPageRef);
+			// The target page will be null when in a missing book
+			Page targetPage;
+			if(targetPageRef.getBook()==null) {
+				targetPage = null;
+			} else if(
+				// Short-cut for element already added above within current page
+				targetPageRef.equals(currentPageRef)
+				&& (
+					element==null
+					|| currentPage.getElementsById().containsKey(element)
+				)
+			) {
+				targetPage = currentPage;
+			} else {
+				// Capture required, even if capturing self
+				// TODO: This would cause unbound recursion and stack overflow at this time, there may be a complicate workaround when needed, such as not running this element on the recursive capture
+				if(targetPageRef.equals(currentPageRef)) throw new NotImplementedException("Forward reference to element in same page not supported yet");
+				targetPage = CapturePage.capturePage(
+					servletContext,
+					request,
+					response,
+					targetPageRef,
+					element==null ? CaptureLevel.PAGE : CaptureLevel.META
+				);
+			}
+			// Find the optional target element, may remain null when in missing book
+			Element targetElement;
+			if(element == null) {
+				if(news.getBook() == null && news.getPage() == null) {
+					if(currentNode instanceof Element) {
+						// Default to current element
+						targetElement = (Element)currentNode;
+					} else {
+						// No current element
+						targetElement = null;
+					}
+				} else {
+					// No element since book and/or page provided
+					targetElement = null;
+				}
+			} else {
+				// Find the element
+				if(targetPage != null) {
+					targetElement = targetPage.getElementsById().get(element);
+					if(targetElement == null) throw new ServletException("Element not found in target page: " + element);
+					if(targetPage.getGeneratedIds().contains(element)) throw new ServletException("Not allowed to link to a generated element id, set an explicit id on the target element: " + element);
+				} else {
+					targetElement = null;
+				}
+			}
+			if(news.getTitle() == null) {
+				String title;
+				if(element != null) {
+					if(targetElement == null) {
+						// Element in missing book
+						title = LinkImpl.getBrokenPath(targetPageRef, element);
+					} else {
+						title = targetElement.getLabel();
+					}
+				} else {
+					if(targetPage == null) {
+						// Page in missing book
+						title = LinkImpl.getBrokenPath(targetPageRef);
+					} else {
+						title = targetPage.getTitle();
+					}
+				}
+				news.setTitle(title);
+			}
+			if(captureLevel == CaptureLevel.BODY) {
+				// Write an empty div so links to this news ID work
+				String refId = PageIndex.getRefIdInPage(servletContext, request, currentPage, news.getId());
+				out.append("<div class=\"semanticcms-news-anchor\" id=\"");
+				encodeTextInXhtmlAttribute(refId, out);
+				out.append("\" />");
+				// TODO: Should we show the news entry here when no news view is active?
+				// TODO: Hide from tree views, or leave but link to "news" view when news view is active?
+			}
+		}
+	}
+
+	/**
+	 * Make no instances.
+	 */
+	private NewsImpl() {
+	}
+}
