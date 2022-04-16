cb93258342714f8e392a4ae504e3f0f90afa60d7
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:41 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/news/servlet/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/news/servlet/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-servlet - SemanticCMS newsfeeds in a Servlet environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-news-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.news.servlet.book;
 
 import com.aoapps.net.URIParametersUtils;
--- src/main/java/com/semanticcms/news/servlet/NewsUtils.java
+++ src/main/java/com/semanticcms/news/servlet/NewsUtils.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-news-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.news.servlet;
 
 import com.semanticcms.core.model.Element;
